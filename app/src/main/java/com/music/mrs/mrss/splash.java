344ca988de8d14106fc30e4cb97287cb19690076
axeronb <budhathoki.amulya@gmail.com>
2015-12-29 14:28:24 +0545

    added splash screen...default icon !!!

--- app/src/main/java/com/music/mrs/mrss/splash.java
+++ app/src/main/java/com/music/mrs/mrss/splash.java
@@ -1,5 +1,6 @@
 package com.music.mrs.mrss;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,17 +14,20 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Thread th = new Thread(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+//                super.run();
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent i = new Intent(splash.this, MainActivity.class);
+                startActivity(i);
             }
-        });
+        };
+        th.start();
     }
 
 }
