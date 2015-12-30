d7b0bf00740bb0533c7267887f110262abb6a894
axeronb <budhathoki.amulya@gmail.com>
2015-12-30 23:38:19 +0545

    added Music Service!!!!

---
+++ app/src/main/java/com/music/mrs/mrss/MusicService.java
@@ -0,0 +1,20 @@
+package com.music.mrs.mrss;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import java.security.Provider;
+
+/**
+ * Created by jusme on 12/30/2015.
+ */
+public class MusicService extends Service {
+
+
+    public IBinder onBind(Intent arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
